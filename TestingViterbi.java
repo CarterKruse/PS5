6b2c8a90b0dec6e14aad182da6182df62a694be4
carterkruse <carterjkruse@gmail.com>
2022-05-24 12:07:51 -0400

    All code finished, commenting complete

---
+++ TestingViterbi.java
@@ -0,0 +1,142 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Testing Viterbi
+ * To assess how good a model is, we can compute how many tags it gets right and how many it gets wrong on some test
+ * sentences.
+ *
+ * The test files use contain sets, one pair with the sentences and a corresponding one with the tags to be used for
+ * training, and another pair with the sentences and tags for testing. Each line is a single sentence (or a headline),
+ * cleanly separated by whitespace into words/tokens, with punctuation also thus separated out.
+ *
+ * We use the train sentences and train tags files to generate the HMM, and then apply it to each line in the test
+ * sentences file, comparing the results to the corresponding test tags line. We count the number of correct vs. incorrect
+ * tag.
+ *
+ * @author Carter Kruse & John DeForest, Dartmouth CS 10, Spring 2022
+ */
+public class TestingViterbi
+{
+    public static void main(String[] args) throws IOException
+    {
+        // BROWN CORPUS
+        // Loading in the testing sentences and tags as "testInput" and "testCompare", respectively.
+        List<List<String>> brownTestInput = InputLibrary.loadSentences("PS5/texts/brown-test-sentences.txt");
+        List<List<String>> brownTestCompare = InputLibrary.loadTags("PS5/texts/brown-test-tags.txt");
+
+        // Training the HMM model for the Viterbi Algorithm based on the Brown corpus.
+        ViterbiAlgorithm.trainModel("PS5/texts/brown-train-sentences.txt", "PS5/texts/brown-train-tags.txt");
+        testFromFiles(brownTestInput, brownTestCompare);
+
+        // SIMPLE FILES
+
+        // List<List<String>> testInput = InputLibrary.loadSentences("PS5/texts/simple-test-sentences.txt");
+        // List<List<String>> testCompare = InputLibrary.loadTags("PS5/texts/simple-test-tags.txt");
+
+        // Training the HMM model for the Viterbi Algorithm based on the simple files.
+        // ViterbiAlgorithm.trainModel("PS5/texts/simple-train-sentences.txt", "PS5/texts/simple-train-tags.txt");
+        // testFromFiles(testInput, testCompare);
+
+        // HARD-CODED STRINGS
+
+        List<List<String>> hardCodedTestInput = new ArrayList<>();
+        List<List<String>> hardCodedTestCompare = new ArrayList<>();
+
+        // List<String> hardCodedTestSentence = Arrays.asList("The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", ".");
+        // List<String> hardCodedTestTags = Arrays.asList("DET", "ADJ", "ADJ", "N", "V", "P", "DET", "ADJ", "N", ".");
+
+        // List<String> hardCodedTestSentence = Arrays.asList("CS", "is", "an", "enjoyable", "course", ".");
+        // List<String> hardCodedTestTags = Arrays.asList("PRO", "V", "DET", "ADJ", "N", ".");
+
+        // List<String> hardCodedTestSentence = Arrays.asList("My", "name", "is", "Carter", "Kruse", ".");
+        // List<String> hardCodedTestTags = Arrays.asList("DET", "N", "V", "NP", "VP", ".");
+
+        // List<String> hardCodedTestSentence = Arrays.asList("The", "purpose", "of", "our", "lives", "is", "to", "be", "happy", ".");
+        // List<String> hardCodedTestTags = Arrays.asList("DET", "N", "P", "PRO", "N", "V", "TO", "V", "ADJ", ".");
+
+        // List<String> hardCodedTestSentence = Arrays.asList("John", "walked", "the", "dog", "through", "the", "neighborhood", ".");
+        // List<String> hardCodedTestTags = Arrays.asList("NP", "VD", "DET", "N", "P", "DET", "N", ".");
+
+        // hardCodedTestInput.add(hardCodedTestSentence);
+        // hardCodedTestCompare.add(hardCodedTestTags);
+
+        // testFromFiles(hardCodedTestInput, hardCodedTestCompare);
+
+        // CONSOLE
+        // testFromConsole();
+    }
+
+    /**
+     * Test From Files - A file-based test method to evaluate the performance on a pair of test files (corresponding
+     * lines with sentences and tags).
+     *
+     * @param observations The List of sentences (containing a List of Strings) of the words from a given input.
+     * @param testTags The List tag groups (containing a List of Strings) of the tags from a given input.
+     */
+    public static void testFromFiles(List<List<String>> observations, List<List<String>> testTags)
+    {
+        double total = 0.0;
+        double matches = 0.0;
+
+        // Cycling through the List of observations.
+        for (int i = 0; i < observations.size(); i += 1)
+        {
+            // Extracting the List of Strings representing the words in a given sentence.
+            List<String> sentence = observations.get(i);
+
+            // Creating a new List of Strings of the tags, based on the sentence.
+            List<String> tags = ViterbiAlgorithm.tagSentence(sentence);
+
+            // Extracting the List of Strings representing the appropriate tags for a given sentence.
+            List<String> givenTags = testTags.get(i);
+
+            // Cycling through the words/tags.
+            for (int j = 0; j < observations.get(i).size(); j += 1)
+            {
+                // If the tag we found using the Viterbi algorithm matches the given tag, we increase the number of matches.
+                if (tags.get(j).equals(givenTags.get(j)))
+                    matches += 1;
+
+                // Regardless, we increase the total number of cases considered.
+                total += 1;
+            }
+        }
+
+        // Printing out the results to the console.
+        System.out.println("Correct: " + matches);
+        System.out.println("Incorrect: " + (total - matches));
+        System.out.println("Percentage: " + (matches * 100 / total));
+    }
+
+    /**
+     * Test From Console - Console-base test method that gives tags from an input line.
+     */
+    public static void testFromConsole()
+    {
+        // Initializing a new Scanner from System.in.
+        Scanner scanner = new Scanner(System.in);
+
+        // Waiting for user input.
+        while (true)
+        {
+            // Prompt and extracting response from user.
+            System.out.println("Enter A Sentence: ");
+            String line = scanner.nextLine().toLowerCase();
+
+            // Quiting the execution of the method.
+            if (line.equals("q"))
+                break;
+
+            // Using the Viterbi algorithm to tag the sentence (words are split by spaces).
+            List<String> consoleTags = ViterbiAlgorithm.tagSentence(List.of(line.split(" ")));
+
+            // Printing out the tags.
+            System.out.println(consoleTags);
+            System.out.println();
+        }
+    }
+}
--- ViterbiAlgorithm.java
+++ ViterbiAlgorithm.java
@@ -3,108 +3,187 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Viterbi Algorithm
+ * The Viterbi algorithm starts at the # (start) state, with a score of 0, before any observation. Then to handle
+ * observation i, it propagates from each reached state at observation i-1, following each transition. The score for
+ * the next state through observation i is the sum of the score at the current state through i-1 plus the transition
+ * score from current to next plus the score of observation i in next.
+ *
+ * We are not going to force a "stop" state (ending with a period, question mark, or exclamation point) since the Brown
+ * corpus includes headlines that break that rule.
+ *
+ * @author Carter Kruse & John DeForest, Dartmouth CS 10, Spring 2022
+ */
 public class ViterbiAlgorithm
 {
     private static HiddenMarkovModel markovModel;
+
+    /* In the case where we do not want to completely rule out something that we have never seen, we give it a low log
+    probability, which is a negative number that is worse than the observed ones, but not totally out of the realm.
+     */
     private static int unseenPenalty = -100;
 
-    public static void trainModel()
+    /**
+     * Train Model
+     * Uses the HiddenMarkovModel class to train the model.
+     *
+     * @param trainSentencesFile The file path for the training sentences.
+     * @param trainTagsFile The file path for the training tags.
+     */
+    public static void trainModel(String trainSentencesFile, String trainTagsFile)
     {
+        // Initializing new ArrayLists which serve to hold all the contents of the training files.
         List<String> words = new ArrayList<>();
         List<String> partsOfSpeech = new ArrayList<>();
 
-        List<List<String>> wordsArray = InputLibrary.loadSentences("PS5/texts/brown-train-sentences.txt");
-        List<List<String>> partsOfSpeechArray = InputLibrary.loadTags("PS5/texts/brown-train-tags.txt");
+        // Using the InputLibrary methods to load the sentences and tags.
+        List<List<String>> wordsArray = InputLibrary.loadSentences(trainSentencesFile);
+        List<List<String>> partsOfSpeechArray = InputLibrary.loadTags(trainTagsFile);
 
+        // Cycling through the wordsArray to add the Strings to the ArrayList.
         for (List<String> item : wordsArray)
         {
             words.addAll(item);
         }
 
+        // Cycling through the partsOfSpeechArray to add the Strings to the ArrayList.
         for (List<String> item : partsOfSpeechArray)
         {
             partsOfSpeech.addAll(item);
         }
 
+        // Creating a new Hidden Markov Model and going through the training.
         markovModel = new HiddenMarkovModel();
         markovModel.trainObservations(partsOfSpeech, words);
-
         markovModel.trainTransitions(partsOfSpeechArray);
     }
 
+    /**
+     * Tag Sentence
+     * A part of speech (POS) tagger labels each word in a sentence with its part of speech (noun, verb, etc.). The goal
+     * of POS tagging is to take a sequence of words and produce the corresponding sequence of tags.
+     *
+     * We only need to keep the current and next scores, which simplifies the representation we use in code.
+     *
+     * @param sentence The sentence to tag with parts of speech.
+     */
     public static List<String> tagSentence(List<String> sentence)
     {
+        // Checking to ensure that the sentence is not empty or null.
         if (sentence == null || sentence.isEmpty())
         {
             System.err.println("Error: Invalid Input");
             return null;
         }
 
-        List<String> currentStates = new ArrayList<String>();
+        // Creating a List of Strings for the currentStates and adding "#".
+        List<String> currentStates = new ArrayList<>();
         currentStates.add("#");
 
-        Map<String, Double> currentScores = new HashMap<String, Double>();
+        // Creating a Map for the currentScores and adding ("#", 0.0).
+        Map<String, Double> currentScores = new HashMap<>();
         currentScores.put("#", 0.0);
 
+        /* The back trace needs to go all the way back: for observation i, for each state, what was the previous state
+        at observation i-1 that produced the best score upon transition and observation.
+         */
         List<Map<String, String>> backTraceList = new ArrayList<>();
 
+        // Extracting the transitionMap and observationMap from the Hidden Markov Model.
         Map<String, Map<String, Double>> transitionMap = markovModel.getTransitionMap();
         Map<String, Map<String, Double>> observationMap = markovModel.getObservationMap();
 
+        // Cycling through each word in the sentence.
         for (int i = 0; i < sentence.size(); i += 1)
         {
+            // Extracting the word in the sentence.
             String word = sentence.get(i);
 
-            List<String> nextStates = new ArrayList<String>();
-            Map<String, Double> nextScores = new HashMap<String, Double>();
+            // Creating a new List of nextStates and Map of nextScores.
+            List<String> nextStates = new ArrayList<>();
+            Map<String, Double> nextScores = new HashMap<>();
 
+            // Adding a new HashMap to the backTraceList.
             backTraceList.add(new HashMap<>());
 
+            // Cycling through each state in the List of current states.
             for (String currentState : currentStates)
             {
-                // Include case in which current state is not in transmission scores.
+                // Check to see if the transitionMap contains the current state. If not, skip this cycle.
                 if (!transitionMap.containsKey(currentState))
                     continue;
 
-                // Transition
+                /* After handling the special case of the start state, we start with the first observation and work
+                forward, considering all possible states from which to come and looking forward to where they could go.
+                 */
+
+                // Cycling through each nextState in the List of nextStates, according to the transitionMap.
                 for (String nextState: transitionMap.get(currentState).keySet())
                 {
+                    // Scoring the next state/observation according to the current score and transition score.
                     double nextScore = currentScores.get(currentState) + transitionMap.get(currentState).get(nextState);
 
+                    // If the observationMap does not contain the next word, we add on the unseenPenalty.
                     if (!observationMap.get(nextState).containsKey(word))
                         nextScore += unseenPenalty;
+
+                    // Otherwise, we simply add on the observation score, given from the observationMap.
                     else
                         nextScore += observationMap.get(nextState).get(word);
 
+                    /* We find the max score (and keep track of which state gave it). Checking to see if the nextScores
+                    Map has a value for the nextState or if the nextScore is greater than the value.
+                     */
                     if (!nextScores.containsKey(nextState) || nextScore > nextScores.get(nextState))
                     {
+                        // Adding the nextState to the List of nextStates.
                         nextStates.add(nextState);
+
+                        // Updating the Map of nextScores.
                         nextScores.put(nextState, nextScore);
+
+                        // Updating the backTraceList to reflect the best score for each observation.
                         backTraceList.get(i).put(nextState, currentState);
                     }
                 }
             }
 
+            // Updating the currentStates and currentScores appropriately.
             currentStates = nextStates;
             currentScores = nextScores;
         }
 
+        // Finding the best last state, according to the final scores.
         String lastState = findMaxEndState(currentScores);
 
+        // Returning the List of tags, according to the backTraceList and lastState.
         return findPath(backTraceList, lastState);
     }
 
+    /**
+     * Find Max End State - Helper function to determine the best tag to use for the last word.
+     *
+     * @param currentScores The Map containing the current scores.
+     */
     public static String findMaxEndState(Map<String, Double> currentScores)
     {
+        // Setting the initial maxScore as low as possible to ensure it is modified.
         double maxScore = Double.NEGATIVE_INFINITY;
+
+        // Initializing the tag that has the best match for the ending observation.
         String bestTagMatch = "";
 
+        // Cycling through the tags in the keySet of the currentScores Map.
         for (String tag : currentScores.keySet())
         {
+            // Extracting the currentScore from the Map.
             double currentScore = currentScores.get(tag);
 
+            // If the currentScore is greater than the maxScore...
             if (currentScore > maxScore)
             {
+                // Update the maxScore and set the bestTagMatch to be the tag.
                 maxScore = currentScore;
                 bestTagMatch = tag;
             }
@@ -113,18 +192,29 @@ public class ViterbiAlgorithm
         return bestTagMatch;
     }
 
+    /**
+     * Find Path - Helper function that allows the back trace to start from the state with the best score for the last
+     * observation, working back to the start state.
+     *
+     * @param backTrackList The List of Maps containing the appropriate transitions from state to state.
+     * @param lastState The lastState to work backward from.
+     */
     public static List<String> findPath(List<Map<String, String>> backTrackList, String lastState)
     {
-        List<String> path = new ArrayList<String>();
+        // Initializing a new ArrayList that will hold the tags (backward path through backTraceList).
+        List<String> path = new ArrayList<>();
 
+        // Ensuring that there is not an OutOfBounds Exception by expanding the ArrayList appropriately.
         for (int i = backTrackList.size() - 1; i >= 0; i -= 1)
-        {
             path.add("");
-        }
 
+        // Cycling through the backTrackList in reverse.
         for (int i = backTrackList.size() - 1; i >= 0; i -= 1)
         {
+            // Setting the lastState to the appropriate index.
             path.set(i, lastState);
+
+            // Updating the lastState using the backTrackList.
             lastState = backTrackList.get(i).get(lastState);
         }
 
